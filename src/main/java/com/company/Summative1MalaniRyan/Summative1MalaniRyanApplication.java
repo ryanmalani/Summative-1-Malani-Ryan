8b77e4d8d5513ac62089ad50b01d2d9f66f08060
Ryan Malani <ryanmalani@Mac-mini.fios-router.home>
2021-09-30 18:46:17 -0400

    first commit

---
+++ src/main/java/com/company/Summative1MalaniRyan/Summative1MalaniRyanApplication.java
@@ -0,0 +1,13 @@
+package com.company.Summative1MalaniRyan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Summative1MalaniRyanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Summative1MalaniRyanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/Summative1MalaniRyan/Summative1MalaniRyanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.Summative1MalaniRyan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Summative1MalaniRyanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
